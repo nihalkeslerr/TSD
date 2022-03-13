204d60a979b641434084d545fd44a0f448d38014
nihalkeslerr <88909415+nihalkeslerr@users.noreply.github.com>
2022-03-13 22:30:34 +0100

    TASK-2
     Leap year method ,generic collection list

---
+++ 02-LINQ/Task2/Task2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task2
+{
+
+
+    class Program
+    {
+        static void Add(int index, string obj, List<string> list)
+        {
+            list.Insert(index, obj);
+        }
+        static string Get(int index, List<string> list)
+        {
+            if (list.Count < index)
+            {
+                Console.Write("Error of Out Of index");
+                return "none";
+            }
+
+            return list[index];
+        }
+        static bool IsEmpty(List<string> list)
+        {
+            if (list.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+
+            //1
+            int[] someYears = new int[] { 2000, 2001, 1994, 1900, 2024, 2025, 2016, 2027, 2028, 1902 };
+            IEnumerable<int> leapYears = someYears.Where(x => (x % 4 == 0 && x % 100 != 0) || (x % 400 == 0));
+
+            Console.WriteLine("Leap years= ");
+            foreach (int year in leapYears) Console.WriteLine(year);
+
+            //2
+            string a = "blue";
+            string b = "green";
+            string c = "purple";
+            string d = "pink";
+
+
+            List<string> ColorList = new List<string>();
+            ColorList.Add(a);
+            ColorList.Add(b);
+
+
+            Add(0, c, ColorList); // Method of add
+            Console.WriteLine(Get(5, ColorList)); // Method of get
+            Console.WriteLine(IsEmpty(ColorList)); //  Method of IsEmpty
+
+
+        }
+
+
+
+    }
+
+}
